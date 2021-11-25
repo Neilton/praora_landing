d94f54e4e27b7b5a53e1733caa17c23cdf2e6d10
Toni Mercadante <mercadanteneto@gmail.com>
2021-11-25 08:28:25 -0300

    navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -8,11 +8,14 @@ import Image from "react-bootstrap/Image";
 import Button from "../Core/Button";
 import { useTranslation } from "react-i18next";
 import { Link as RLink } from "react-scroll";
-import { isMobile } from 'react-device-detect';
+import { isMobile } from "react-device-detect";
+import { useState } from "react";
 
 function NavBar() {
   const [t] = useTranslation();
-  
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+
   return (
     <Navbar
       fixed="top"
@@ -31,27 +34,35 @@ function NavBar() {
           </Navbar.Brand>
 
           <Navbar.Collapse id="responsive-navbar-nav">
-            <div className={`${styles.navWrp}`}>
-              <Nav.Item>
+            <div className={isOpen ? `${styles.navWrp} ${styles.activated}` : styles.navWrap}>
+             <Nav.Item>
                 <Nav.Link className={styles.navItem}>
-                <RLink
+                  <RLink
                     to="WhatIsPraora"
-                    spy={true} smooth={true} offset={50} duration={600} delay={100}
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={600}
+                    delay={100}
                     className={styles.navLink}
                   >
-                     {t('WhatIsPraora')}
+                    {t("WhatIsPraora")}
                   </RLink>
                 </Nav.Link>
               </Nav.Item>
               <Nav.Link className={styles.navItem}>
                 <RLink
-                    to="resources"
-                    spy={true} smooth={true} offset={50} duration={600} delay={100}
-                    className={styles.navLink}
-                  >
-                     {t('resources')}
-                  </RLink>
-                </Nav.Link>
+                  to="resources"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={600}
+                  delay={100}
+                  className={styles.navLink}
+                >
+                  {t("resources")}
+                </RLink>
+              </Nav.Link>
               <Nav.Item>
                 <Nav.Link className={styles.navItem}>
                   <a
@@ -59,28 +70,47 @@ function NavBar() {
                     data-scroll="howtobeg"
                     className={styles.navLink}
                   >
-                    {t('roadmap')}
+                    {t("roadmap")}
                   </a>
                 </Nav.Link>
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link className={styles.navItem}>
-                <RLink
+                  <RLink
                     to="contact"
-                    spy={true} smooth={true} offset={50} duration={600} delay={100}
-                    className={styles.navLink}>{t('contact')}
-                    </RLink>
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={600}
+                    delay={100}
+                    className={styles.navLink}
+                  >
+                    {t("contact")}
+                  </RLink>
                 </Nav.Link>
               </Nav.Item>
             </div>
           </Navbar.Collapse>
-          {!isMobile && <a href={"https://docs.praora.com/whitepaper" } aria-label='Whitepaper' target="_blank" rel="noreferrer"> <Button className={styles.headerBtn}>
-            {t('whitepaper')}
-          </Button>
-          </a>}
-          <Navbar.Toggle className={styles.toggleBar}>
+          {!isMobile && (
+            <a
+              href={"https://docs.praora.com/whitepaper"}
+              aria-label="Whitepaper"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.headerBtnLink}
+
+            >
+              {" "}
+              <Button className={styles.headerBtn}>{t("whitepaper")}</Button>
+            </a>
+          )}
+          <div className={styles.toggleBar} onClick={handleClick}>
+            <Image src={bar} fluid alt="" />
+          </div>
+
+          {/* <Navbar.Toggle className={styles.toggleBar}>
             <Image src={bar} className="img-fluid" alt="" />
-          </Navbar.Toggle>
+          </Navbar.Toggle> */}
         </div>
       </Container>
     </Navbar>
