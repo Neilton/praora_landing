fdbb9af7486c5cedd646d4b341db3c18066c82c1
Toni Mercadante <mercadanteneto@gmail.com>
2021-11-29 01:03:10 -0300

    captcha

--- src/components/Contact/IsolateContact/index.tsx
+++ src/components/Contact/IsolateContact/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import Rafiki from "../../../assets/img/rafiki.png";
 import Slack from "../../../assets/img/icon/slack.png";
 import Instagram from "../../../assets/img/icon/instagram.png";
@@ -8,9 +8,9 @@ import styles from "./styles.module.scss";
 import Button from "../../Core/Button";
 import { useForm } from "react-hook-form";
 import ReCAPTCHA from "react-google-recaptcha";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 type FormData = {
   fullName: string;
@@ -20,44 +20,51 @@ type FormData = {
 
 function Contact() {
   const [t] = useTranslation();
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const recaptchaRef = useRef<ReCAPTCHA>(null);
   const ReCAPTCHAKey = "6LcMyl4dAAAAANJB0l6a5GJcLVxW7AO1YH3eXfat";
-  const onSubmit = handleSubmit(async data => {
-    const api_key = "UEESivQTrbbYQFwnwBCjbVvjJAWDqYxpCcqt"
-    const url = `https://api.rd.services/platform/conversions?api_key="${api_key}"`;
+  // @ts-ignore
+  async function onSubmit(data, e) {
+    e.preventDefault();
+    onSubmitWithReCAPTCHA(e);
+
+    const api_key = "UEESivQTrbbYQFwnwBCjbVvjJAWDqYxpCcqt";
+    const url = `https://api.rd.services/platform/conversions?api_key=${api_key}`;
 
     const request = {
       event_type: "CONVERSION",
       event_family: "CDP",
-      "payload": {
+      payload: {
         conversion_identifier: "Landing Page Form",
         name: data.fullName,
         email: data.email,
-        cf_message: data.message
-      }
-    }
+        cf_message: data.message,
+      },
+    };
 
-    try {
-      const resp = await axios.post(url, request)
-      const { status } = await resp;
-      status === 200 ? toast.success(t("ThanksForYourInteresting")) : toast.error(t("HumSomethingGoesWrong"))
-    } catch (err) {
-      toast.error(t("HumSomethingGoesWrong"), { autoClose: 5000 })
-    }
-  });
+    const resp = await axios.post(url, request);
+    const { status } = resp;
+    console.log("SOU A RESP", resp.status);
+    status === 200
+      ? toast.success(t("ThanksForYourInteresting"))
+      : toast.error(t("HumSomethingGoesWrong"));
+  }
 
   const onSubmitWithReCAPTCHA = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
     try {
       if (recaptchaRef.current !== null) {
+        console.log("REACPATHAA", recaptchaRef.current);
         const _result = await recaptchaRef.current.executeAsync();
         console.log(_result);
       }
     } catch (err) {
       toast.error(t("ReCAPTCHA"));
     }
-  }
+  };
 
   function redoCaptcha() {
     if (recaptchaRef.current !== null) {
@@ -66,64 +73,126 @@ function Contact() {
     }
   }
 
-  return <React.Fragment>
-    <section className={styles.contactWrap} id="contact">
-      <ToastContainer />
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-7">
-            <div className={styles.contactContent}>
-              <h4>{t('ContactUs')}</h4>
-              <p>{t('QuestionOrSuggestions')}</p>
-              <img src={Rafiki} alt="" className="img-fluid" />
-            </div>
-          </div>
-          <div className="col-lg-5">
-            <div className={styles.contactForm}>
-              <h6>{t('SocialMedias')}</h6>
-              <ul className={styles.socialWrap}>
-                <li><a href="https://praora.slack.com/messages/general/" target="__blank" rel="noopen"><img src={Slack} alt="Slack" /></a></li>
-                <li><a href="https://www.instagram.com/_praora/" target="__blank" rel="noopen"><img src={Instagram} alt="Instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/company/praora" target="__blank" rel="noopen"><img src={Linkedin} alt="Linkedin" /></a></li>
-              </ul>
-              <div className={`${styles.formHeading} position-relative`}>
-                <hr className={styles.formHr} />
-                <h6 className="mb-0">{t('SendUsMessage')}</h6>
-                <hr className={styles.formHr} />
+  return (
+    <React.Fragment>
+      <section className={styles.contactWrap} id="contact">
+        <ToastContainer />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7">
+              <div className={styles.contactContent}>
+                <h4>{t("ContactUs")}</h4>
+                <p>{t("QuestionOrSuggestions")}</p>
+                <img src={Rafiki} alt="" className="img-fluid" />
               </div>
-              <div className="d-flex justify-content-center">
-                <form onSubmit={onSubmitWithReCAPTCHA}>
+            </div>
+            <div className="col-lg-5">
+              <div className={styles.contactForm}>
+                <h6>{t("SocialMedias")}</h6>
+                <ul className={styles.socialWrap}>
+                  <li>
+                    <a
+                      href="https://praora.slack.com/messages/general/"
+                      target="__blank"
+                      rel="noopen"
+                    >
+                      <img src={Slack} alt="Slack" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/_praora/"
+                      target="__blank"
+                      rel="noopen"
+                    >
+                      <img src={Instagram} alt="Instagram" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/company/praora"
+                      target="__blank"
+                      rel="noopen"
+                    >
+                      <img src={Linkedin} alt="Linkedin" />
+                    </a>
+                  </li>
+                </ul>
+                <div className={`${styles.formHeading} position-relative`}>
+                  <hr className={styles.formHr} />
+                  <h6 className="mb-0">{t("SendUsMessage")}</h6>
+                  <hr className={styles.formHr} />
+                </div>
+                <div className="d-flex justify-content-center">
+                  {/* @ts-ignore */}
+                  <form onSubmit={handleSubmit(onSubmit)}>
                     <div className={styles.formContent}>
                       <div className={styles.formGroup}>
-                        <label htmlFor="name">{t('YourName')}</label>
-                        <input type="text" className="form-control" id="name" placeholder={t('EgJohnnyBravo')} {...register("fullName", { required: true })} />
-                        <p className="text-info">{errors.fullName?.type === 'required' && t('FirstNameRequired')}</p>
+                        <label htmlFor="name">{t("YourName")}</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="name"
+                          placeholder={t("EgJohnnyBravo")}
+                          {...register("fullName", { required: true })}
+                        />
+                        <p className="text-info">
+                          {errors.fullName?.type === "required" &&
+                            t("FirstNameRequired")}
+                        </p>
                       </div>
                       <div className={styles.formGroup}>
-                        <label htmlFor="InputEmail1">{t('YourEmail')}</label>
-                        <input type="email" className="form-control" id="InputEmail1" placeholder={t('EmailSample')} {...register("email", { required: true })} />
-                        <p className="text-info">{errors.email && t('EmailRequired')}</p>
+                        <label htmlFor="InputEmail1">{t("YourEmail")}</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="InputEmail1"
+                          placeholder={t("EmailSample")}
+                          {...register("email", { required: true })}
+                        />
+                        <p className="text-info">
+                          {errors.email && t("EmailRequired")}
+                        </p>
                       </div>
                       <div className={styles.formGroup}>
-                        <label htmlFor="textarea">{t('YourMessage')}</label>
-                        <textarea className="form-control" id="textarea" placeholder={t('Message')} defaultValue={""} {...register("message")} />
-                        <p className="text-info">{errors.message && t('MessageWrong')}</p>
+                        <label htmlFor="textarea">{t("YourMessage")}</label>
+                        <textarea
+                          className="form-control"
+                          id="textarea"
+                          placeholder={t("Message")}
+                          defaultValue={""}
+                          {...register("message")}
+                        />
+                        <p className="text-info">
+                          {errors.message && t("MessageWrong")}
+                        </p>
                       </div>
-                      <Button>{t('SendMessage')}</Button>
+                      <Button>{t("SendMessage")}</Button>
                     </div>
                     <div className="mt-4">
-                      <ReCAPTCHA ref={recaptchaRef} sitekey={ReCAPTCHAKey} size="invisible" 
-                      badge="inline" onExpired={redoCaptcha} onErrored={() => toast.error(t("HumSomethingGoesWrong"), { autoClose: 5000 })} 
-                      onChange={() => onSubmit}
-                      /></div>
-                </form>
+                      <ReCAPTCHA
+                        ref={recaptchaRef}
+                        sitekey={ReCAPTCHAKey}
+                        size="invisible"
+                        badge="inline"
+                        onExpired={redoCaptcha}
+                        onErrored={() =>
+                          toast.error(t("HumSomethingGoesWrong"), {
+                            autoClose: 5000,
+                          })
+                        }
+                        // onChange={() => onSubmit}
+                      />
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </React.Fragment>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default Contact;
