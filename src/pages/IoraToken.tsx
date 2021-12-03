a966c88321c07d094645e70263609d37d18ed84e
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2021-12-03 13:14:36 -0300

    IoraToken

--- src/pages/IoraToken.tsx
+++ src/pages/IoraToken.tsx
@@ -15,178 +15,150 @@ import Mockup4 from "../assets/img/token/mockup-4.png"
 import Elipse3 from "../assets/img/token/circle/elipses-3.png"
 import Elipse2 from "../assets/img/token/circle/elipses-2.png"
 import Phone from "../assets/img/token/telefone.png"
+import { useTranslation } from "react-i18next"
 
 function IoraToken() {
+    const [t] = useTranslation();
 
-  return <section id="IoraToken">
-          <div className="token-page-wrp position-relative">
-        <img src={Eclipse} alt="" className="token-bg tkbg1" />
-        <div className="iora-container">
-          <div className="iora-plat-wrp text-center">
-            <h2>Iora: O token para <br /> prestadores de serviços</h2>
-            <p>Iora é o token da Praora e além de trazer a experiência de uma <br /> plataforma descentralizada, ainda
-              te dá benefícios incríveis para produtores de conteúdo.</p>
-            <a href="#" className="iora-btns">Comprar tokens</a>
-            <div className="iora-cont-address iora-top-contact">
-              <h5>Iora Contract Adress:</h5>
-              <div className="iroa-add-input">
-                <input type="text" defaultValue="0xee4c36aaf349ca458deBbD94dFdd66dC941C6616" />
-                <div className="iora-add-contact">
-                  <a href="#"><img src={CopyContent} alt="Copycontent" /></a>
-                  <a href="#"><img src={Metamask} alt="Use Metamask" /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="iora-imagem d-flex justify-content-center">
-          <div className="iora-imagem-box left-imagem">
-            <img src={Image1} className="img-fluid" alt="" />
-          </div>
-          <div className="iora-imagem-box right-imagem">
-            <img src={Image2} className="img-fluid" alt="" />
-          </div>
-        </div>
-        <div className="section-bg-one position-relative">
-          <img src={Eclipse2} alt="" className="token-bg tkbg2" />
-          <div className="iora-container">
-            <div className="iora-invest-wrp">
-              <h3>Invista em Iora e tenha uma <br /> experiência descentralizada</h3>
-              <div className="invest-row">
-                <div className="iora_invest_box text-center">
-                  <div>
-                    <div className="iora-invest-icon">
-                      <img src={LiveTV} alt="" className="img-fluid" />
-                    </div>
-                    <h6>Faça lives <br /> facilmente</h6>
-                  </div>
-                </div>
-                <div className="iora_invest_box text-center">
-                  <div>
-                    <div className="iora-invest-icon">
-                      <img src={Wallet} alt="Wallet" className="img-fluid" />
-                    </div>
-                    <h6>Conecte <br /> sua carteira</h6>
-                  </div>
-                </div>
-                <div className="iora_invest_box text-center">
-                  <div>
-                    <div className="iora-invest-icon">
-                      <img src={Union} alt="" className="img-fluid" />
-                    </div>
-                    <h6>Totalmente <br /> descentralizada</h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="investing-agora">
-              <h3>A hora de investir é agora!</h3>
-              <p>
-                O mercado de tokens e criptomoedas nunca esteve tão aquecido como agora. Cada vez mais, o
-                mercado
-                financeiro se interessa mais pelo assunto, visto que um dos maiores diferenciais dessa área é a
-                tecnologia trazida pelo blockchain que evitam fraudes e deixam as transações mais seguras. Além
-                de
-                contar com toda a segurança da blockchain, os tokens ainda trazem benefícios para os portadores.
-              </p>
-              <p>
-                Ao comprar a Iora, os portadores serão beneficiados com repasse de 3% no valor de transações e
-                ainda
-                usaremos uma porcentagem das transações para investimento visando a expansão da plataforma.
-                Acesse
-                nosso <b>WhitePaper</b> e veja todos os detalhes sobre a Iora
-              </p>
-            </div>
-            <div className="iora-mockup">
-              <div className="iora-mockup-box">
-                <img src={Mockup1} alt="" className="img-fluid" />
-              </div>
-              <div className="iora-mockup-box">
-                <img src={Mockup2} alt="" className="img-fluid" />
-              </div>
-              <div className="iora-mockup-box">
-                <img src={Mockup3} alt="" className="img-fluid" />
-              </div>
-              <div className="iora-mockup-box">
-                <img src={Mockup4} alt="" className="img-fluid" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="sec-bg-tf position-relative">
-          <img src={Elipse3} alt="" className="token-bg tkbg3" />
-          <img src={Elipse2} alt="" className="token-bg tkbg4" />
-          <div className="iora-container">
-            <div className="iora-item-cont iora-item-secure">
-              <h4>Descentralizado e seguro</h4>
-              <p>
-                A Praora oferece uma estrutura totalmente descentralizada e segura pra usuários que não querem
-                expor
-                seus dados pessoais. Oferecemos uma plataforma para recebimentos e pagamentos por criptomoedas
-                de
-                forma instantânea e eficiente.
-              </p>
-            </div>
-            <div className="iora-item-cont iora-item-start-live">
-              <h4>Inicie lives de <br /> forma descomplicada</h4>
-              <p>
-                Queremos que qualquer usuário possa iniciar uma live de forma totalmente descomplicada e sem
-                nenhuma
-                burocracia. Nosso foco é dar a oportunidade para criadores de conteúdos poderem facilmente ter
-                um
-                canal de streaming monetizado através de doações, inscrições e publicidades.
-              </p>
-            </div>
-            <div className="iora-item-cont iora-item-connect">
-              <h4>Conecte sua Carteira</h4>
-              <p>
-                Os usuários da Praora podem conectar qualquer carteira de cripto dentro da plataforma e podem,
-                inclusive, utilizar a nossa carteira, PraWallet. Também é possível sincronizar sua conta no
-                Metamask
-                e assim usufruir de toda a segurança e benefícios que uma rede descentralizada pode oferecer.
-              </p>
-            </div>
-            <div className="iora-item-cont iora-item-uma-platf">
-              <h4>Uma plataforma <br /> totalmente descentralizada</h4>
-              <p>
-                47% dos usuários tem um receio em compartilhar dados pessoais dentro de uma plataforma e por
-                isso,
-                optam por utilizar nomes ficticios para manter a privacidade. Nós da Praora, nos preocupamos
-                bastante com a segurança dos dados dos nossos usuários, portanto, oferecemos uma plataforma
-                descentralizada, segura e totalmente livre de censuras governamentais. Você pode acessar nossas
-                regras de forma totalmente transparente clicando aqui
-              </p>
-            </div>
-            <div className="uma-sec-platf-wrp">
-              <div className="uma-platform-box">
-                <div className="uma-platform-content">
-                  <div className="uma-platform-left">
-                    <h3>Faça parte de uma <br /> plataforma descentralizada</h3>
-                    <p>Invista na Iora e tenha a experiência de fazer parte de <br /> uma plataforma
-                      totalmente
-                      descentralizada </p>
-                    <a href="#" className="iora-btns">Comprar tokens</a>
-                    <div className="iora-cont-address iora-bottm-contact">
-                      <h5>Iora Contract Adress:</h5>
-                      <div className="iroa-add-input">
-                        <input type="text" defaultValue="0xee4c36aaf349ca458deBbD94dFdd66dC941C6616" />
-                        <div className="iora-add-contact">
-                          <a href="#"><img src={CopyContent} alt="" /></a>
-                          <a href="#"><img src={Metamask} alt="" /></a>
+    return <section id="IoraToken">
+        <div className="token-page-wrp position-relative">
+            <img src={Eclipse} alt="" className="token-bg tkbg1" />
+            <div className="iora-container">
+                <div className="iora-plat-wrp text-center">
+                    <h2>{t("Iora.Part1")}<br /> {t("Iora.Part2")}</h2>
+                    <p>{t("Iora.Part3")} <br /> {t("Iora.Part4")}</p>
+                    <a href="#" className="iora-btns">{t("BuyIora")}</a>
+                    <div className="iora-cont-address iora-top-contact">
+                        <h5>{t("ContractAddress")}</h5>
+                        <div className="iroa-add-input">
+                            <input type="text" defaultValue="0xee4c36aaf349ca458deBbD94dFdd66dC941C6616" />
+                            <div className="iora-add-contact">
+                                <a href="#"><img src={CopyContent} alt="Copycontent" /></a>
+                                <a href="#"><img src={Metamask} alt="Use Metamask" /></a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div className="uma-platform-right">
-                    <img src={Phone} alt="" className="img-fluid" />
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
+            <div className="iora-imagem d-flex justify-content-center">
+                <div className="iora-imagem-box left-imagem">
+                    <img src={Image1} className="img-fluid" alt="" />
+                </div>
+                <div className="iora-imagem-box right-imagem">
+                    <img src={Image2} className="img-fluid" alt="" />
+                </div>
+            </div>
+            <div className="section-bg-one position-relative">
+                <img src={Eclipse2} alt="" className="token-bg tkbg2" />
+                <div className="iora-container">
+                    <div className="iora-invest-wrp">
+                        <h3>{t("SectionInvesting.Part1")} <br /> {t("SectionInvesting.Part2")}</h3>
+                        <div className="invest-row">
+                            <div className="iora_invest_box text-center">
+                                <div>
+                                    <div className="iora-invest-icon">
+                                        <img src={LiveTV} alt="" className="img-fluid" />
+                                    </div>
+                                    <h6>{t("TokenFeatures.Part1")} <br /> {t("TokenFeatures.Part2")}</h6>
+                                </div>
+                            </div>
+                            <div className="iora_invest_box text-center">
+                                <div>
+                                    <div className="iora-invest-icon">
+                                        <img src={Wallet} alt="Wallet" className="img-fluid" />
+                                    </div>
+                                    <h6>{t("TokenFeatures.Part3")} <br /> {t("TokenFeatures.Part4")}</h6>
+                                </div>
+                            </div>
+                            <div className="iora_invest_box text-center">
+                                <div>
+                                    <div className="iora-invest-icon">
+                                        <img src={Union} alt="" className="img-fluid" />
+                                    </div>
+                                    <h6>{t("TokenFeatures.Part5")} <br /> {t("TokenFeatures.Part6")}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="investing-agora">
+                        <h3>{t("Reasons.Part1")}</h3>
+                        <p>{t("Reasons.Part2")}</p>
+                        <p>{t("Reasons.Part3")}</p>
+                        <p>{t("Reasons.Part4")}<a href="https://docs.praora.com"><b>{t("whitepaper")}</b></a> {t("Reasons.Part5")}
+                        </p>
+                    </div>
+                    <div className="iora-mockup">
+                        <div className="iora-mockup-box">
+                            <img src={Mockup1} alt="" className="img-fluid" />
+                        </div>
+                        <div className="iora-mockup-box">
+                            <img src={Mockup2} alt="" className="img-fluid" />
+                        </div>
+                        <div className="iora-mockup-box">
+                            <img src={Mockup3} alt="" className="img-fluid" />
+                        </div>
+                        <div className="iora-mockup-box">
+                            <img src={Mockup4} alt="" className="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="sec-bg-tf position-relative">
+                <img src={Elipse3} alt="" className="token-bg tkbg3" />
+                <img src={Elipse2} alt="" className="token-bg tkbg4" />
+                <div className="iora-container">
+                    <div className="iora-item-cont iora-item-secure">
+                        <h4>{t("Reasons.Part6")}</h4>
+                        <p>
+                            {t("Reasons.Part7")}
+                        </p>
+                    </div>
+                    <div className="iora-item-cont iora-item-start-live">
+                        <h4>{t("Reasons.Part8")}<br /> {t("Reasons.Part9")}</h4>
+                        <p>
+                            {t("Reasons.Part10")}
+                        </p>
+                    </div>
+                    <div className="iora-item-cont iora-item-connect">
+                        <h4> {t("Reasons.Part11")}</h4>
+                        <p>
+                        {t("Reasons.Part12")}
+                        </p>
+                    </div>
+                    <div className="iora-item-cont iora-item-uma-platf">
+                        <h4> {t("Reasons.Part13")} <br />  {t("Reasons.Part14")}</h4>
+                        <p>
+                        {t("Reasons.Part15")}
+                        </p>
+                    </div>
+                    <div className="uma-sec-platf-wrp">
+                        <div className="uma-platform-box">
+                            <div className="uma-platform-content">
+                                <div className="uma-platform-left">
+                                    <h3>{t("Community.Part1")} <br /> {t("Community.Part2")}</h3>
+                                    <p>{t("Community.Part3")} <br />{t("Community.Part4")}</p>
+                                    <a href="#" className="iora-btns">{t("BuyIora")}</a>
+                                    <div className="iora-cont-address iora-bottm-contact">
+                                        <h5>{t("ContractAddress")}</h5>
+                                        <div className="iroa-add-input">
+                                            <input type="text" defaultValue="0xee4c36aaf349ca458deBbD94dFdd66dC941C6616" />
+                                            <div className="iora-add-contact">
+                                                <a href="#"><img src={CopyContent} alt="" /></a>
+                                                <a href="#"><img src={Metamask} alt="" /></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="uma-platform-right">
+                                    <img src={Phone} alt="" className="img-fluid" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-</section>
+    </section>
 }
 
 export default IoraToken;
